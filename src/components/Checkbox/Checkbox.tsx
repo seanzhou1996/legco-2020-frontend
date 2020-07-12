e291556216ddc94317c437d25f3b48d2e528b448
Sean Zhou <seanzhousiyuan@outlook.com>
2020-07-12 23:41:04 +0800

    added checkbox styling

--- src/components/Checkbox/Checkbox.tsx
+++ src/components/Checkbox/Checkbox.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import './Checkbox.scss';
+
 export interface CheckboxProps {
   id: string,
   name: string,
@@ -13,12 +15,13 @@ class Checkbox extends React.Component<CheckboxProps> {
     return (
       <div className="legco-checkbox">
         <input 
+          className="legco-checkbox__input"
           type="checkbox" 
           name={ this.props.name } 
           id={ this.props.id } 
           checked={ this.props.checked }
           onChange={ this.props.onChange } />
-        <label htmlFor={ this.props.id }>
+        <label className="legco-checkbox__label" htmlFor={ this.props.id }>
           { this.props.label }
         </label>
       </div>
