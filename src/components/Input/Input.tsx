d628a61fc82019519fc0f874fc3a5095753b4308
Sean Zhou <seanzhousiyuan@outlook.com>
2020-07-21 20:55:21 +0800

    converted component to a forwardRef, removed focus, blur event handlers

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -5,30 +5,24 @@ import './Input.scss';
 
 import { Props } from '../../models';
 
-interface InputProps extends Props {
+export interface InputProps extends Props {
   type?: 'text' | 'number',
   name?: string,
   id?: string,
   value?: string,
   placeholder?: string,
-  onChange?: React.ChangeEventHandler,
-  onFocus?: React.FocusEventHandler,
-  onBlur?: React.FocusEventHandler
+  onChange?: React.ChangeEventHandler<HTMLInputElement>
 }
 
-class Input extends React.Component<InputProps> {
-  static defaultProps: InputProps = {
-    type: 'text'
-  }
-  render() {
-    const inputClass = classnames(
-      ...(this.props.className || '').split(/\s+/g),
-      'legco-input'
-    );
-    return (
-      <input {...this.props} className={ inputClass } />
-    );
-  }
-}
+export type InputRef = HTMLInputElement;
 
-export default Input;
+export default React.forwardRef<InputRef, InputProps>((props = { type: 'text' }, ref) => {
+  const { className, children, ..._props } = props;
+  const inputClass = classnames(
+    ...(className || '').split(/\s+/g),
+    'legco-input'
+  );
+  return (
+    <input {..._props} className={ inputClass } ref={ ref } />
+  )
+});
