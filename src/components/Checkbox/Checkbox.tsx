aca53c310364d6527551a8c6bd66c81ea572cfa1
Sean Zhou <seanzhousiyuan@outlook.com>
2020-09-06 16:59:21 +0800

    add `size` props

--- src/components/Checkbox/Checkbox.tsx
+++ src/components/Checkbox/Checkbox.tsx
@@ -1,24 +1,23 @@
 import React from 'react';
 import classnames from 'classnames';
 
-import { Props } from 'constants/types';
-
 import './Checkbox.scss';
 
-export interface CheckboxProps extends Props {
+export interface CheckboxProps {
   id: string,
   name: string,
   label: string,
   checked: boolean,
-  onChange: React.ChangeEventHandler<HTMLInputElement>
+  onChange: React.ChangeEventHandler<HTMLInputElement>,
+  size?: 'small',
 }
 
 class Checkbox extends React.Component<CheckboxProps> {
   render() {
-    const { className, label, ...inputProps } = this.props;
+    const { size, label, ...inputProps } = this.props;
     const checkboxClass = classnames(
-      ...(className || '').split(/\s+/g),
-      'legco-checkbox'
+      'legco-checkbox',
+      size === 'small' ? 'legco-checkbox--small' : null,
     );
     return (
       <div className={ checkboxClass }>
